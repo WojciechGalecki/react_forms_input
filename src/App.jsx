d327403da9db4c31b052ed66ebe50bccb64cdc6f
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-12-13 12:00:23 +0100

    form actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from './components/Header.jsx';
 //import Login from './components/Login-state.jsx';
-import Login from './components/Login-state-hook.jsx';
+//import Login from './components/Login-state-hook.jsx';
 //import Login from './components/Login.jsx';
-import Signup from './components/Signup.jsx';
+//import Signup from './components/Signup.jsx';
+import Signup from './components/Signup-form-actions.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        {/* <Signup /> */}
-        <Login />
+        <Signup />
+        {/* <Login /> */}
       </main>
     </>
   );
