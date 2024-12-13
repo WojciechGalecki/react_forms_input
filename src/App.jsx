5285982153582d8938bffc51320e32b269ee879c
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-12-13 11:00:10 +0100

    custom validation hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login-state.jsx';
+//import Login from './components/Login-state.jsx';
+import Login from './components/Login-state-hook.jsx';
 //import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 
---
+++ src/components/Login-state-hook.jsx
@@ -0,0 +1,66 @@
+import Input from "./Input";
+import { hasMinLength, isEmail, isNotEmpty } from "../util/validation";
+import { useInput } from "../hooks/useInput";
+
+export default function Login() {
+  const {
+    value: email,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+
+  const {
+    value: password,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 3));
+
+  function handleSubmit(event) {
+    event.preventDefault(); // prevent http request made by the browser
+
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
+          value={email}
+          error={emailHasError && "Please enter a valid email"}
+        />
+
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
+          value={password}
+          error={passwordHasError && "Please enter a valid password"}
+        />
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        {/* <button type="button" className="button" onClick={handleSubmit}> */}
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
